73132d58b20275919106e19db95ba6dadb51b547
Trwifon <trifon@mail.com>
2024-12-16 23:27:08 +0200

    added accounts and common

--- nadejda_94_django/settings.py
+++ nadejda_94_django/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'nadejda_94_django.accounts',
+    'nadejda_94_django.common'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
